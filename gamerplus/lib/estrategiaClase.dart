eae52017269a193c16a6c1a88fea9262adfea0f6
Vaporu-Code <jpacevedob@gmail.com>
2024-07-04 18:53:32 -0400

    clases añadidas

---
+++ gamerplus/lib/estrategiaClase.dart
@@ -0,0 +1,14 @@
+import 'dart:collection';
+import 'dart:ffi';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class estrategias {
+  bool completado;
+  String descripcion;
+  String imagen;
+
+  estrategias(this.completado, this.descripcion, this.imagen);
+
+}
---
+++ gamerplus/lib/juegoClase.dart
@@ -0,0 +1,19 @@
+import 'dart:collection';
+import 'dart:ffi';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class juegos {
+  String imagen;
+  String nombre;
+  int anno;
+  String genero;
+  String descripcion;
+  String link;
+  Float puntuacion;
+  String worldRecord;
+
+  juegos(this.imagen, this.nombre, this.anno, this.genero, this.descripcion, this.link, this.puntuacion, this.worldRecord);
+
+}
